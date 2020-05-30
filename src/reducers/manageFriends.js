c7c74b7ee5c4e06b738c428f2d44e8e93703ed6c
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-30 17:49:08 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+   let newState= {}
+    switch(action.type){
+        case 'ADD_FRIEND':
+            let friendArray = [...state.friends]
+            friendArray.push(action.friend)
+            newState['friends'] = friendArray
+            return newState
+        case 'REMOVE_FRIEND':
+            let removeFriendArray = [...state.friends]
+            newState['friends'] = removeFriendArray.filter(f => f.id !== action.id)
+            return newState
+    }
+   return state 
 }
